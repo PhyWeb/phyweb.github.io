1074a065e648fc2507246e5ec385b1b93c5a4cd5
Gaétan Walter <174212685+PhyWeb@users.noreply.github.com>
2024-11-22 21:53:39 +0100

    electron

--- forge.config.js
+++ forge.config.js
@@ -4,6 +4,7 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
+    ignore: "/portable",
     icon: '/common/assets/phyweb'
   },
   rebuildConfig: {},
