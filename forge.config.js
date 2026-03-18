e01aef45147531acb720d44cd302d7bb9941e2a2
Gaétan Walter <174212685+PhyWeb@users.noreply.github.com>
2026-03-18 14:09:30 +0100

    APPX
    ajout d'une video

--- forge.config.js
+++ forge.config.js
@@ -25,7 +25,9 @@ module.exports = {
       config: {
         identityName: 'PhyWeb.PhyWeb', // Le "Package/Identity/Name" du Partner Center
         publisher: 'CN=2A272021-41C4-4363-BC75-191E2B3B681C', // Le "Package/Identity/Publisher" du Partner Center
-        publisherDisplayName: 'PhyWeb' // Le "PublisherDisplayName"
+        publisherDisplayName: 'PhyWeb', // Le "PublisherDisplayName"
+        languages: ['fr-FR'], // Les langues supportées par l'application (BUG ne semble pas être pris en compte )
+        assets: './assets/appx'
       }
     },
     /*{
