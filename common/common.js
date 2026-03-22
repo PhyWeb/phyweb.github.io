0ed1453f80b15d6bae2776172aadb721e0498aa4
Gaétan Walter <174212685+PhyWeb@users.noreply.github.com>
2026-03-22 14:50:37 +0100

    Bump to v0.1.9

--- common/common.js
+++ common/common.js
@@ -388,7 +388,7 @@ function aboutModal(_app){
   // electron
   let version = "";
   if(window.electronAPI){
-    version = "v0.1.8";
+    version = "v0.1.9";
   }
   // Description
   let description;
