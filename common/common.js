ff6400862b7ffa36b7e5bccfcd62227a03bd6f00
Gaétan Walter <174212685+PhyWeb@users.noreply.github.com>
2026-07-19 18:36:30 +0200

    bump to v0.2.13

--- common/common.js
+++ common/common.js
@@ -408,7 +408,7 @@ function aboutModal(_app){
   let platformInfo = "";
 
   if(window.electronAPI){
-    version = "v0.2.12";
+    version = "v0.2.13";
     // Affichage spécifique à l'application Electron
     platformInfo = `
         <div class="field is-horizontal">
