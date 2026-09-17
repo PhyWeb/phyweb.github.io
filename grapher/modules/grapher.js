b2f902f69396261a8cf70d90534a31345775c59d
Gaétan Walter <174212685+PhyWeb@users.noreply.github.com>
2026-09-17 09:23:47 +0200

    bugfix: affichage des modeles lors d'un zoom

--- grapher/modules/grapher.js
+++ grapher/modules/grapher.js
@@ -416,6 +416,21 @@ export default class Grapher {
         maxPadding: 0,
         minRange: 1e-30, // Pour éviter les zooms extrêmes
         events: {
+          setExtremes: function (e) {
+            const grapher = this.chart.options.customGrapherInstance;
+            if (grapher && !grapher.suppressModelAutoUpdate) {
+              // On récupère les nouvelles bornes du zoom (e.min et e.max)
+              // Si c'est un "Reset Zoom" (null), on prend les bornes globales des données
+              let newMin = e.min !== undefined && e.min !== null ? e.min : this.dataMin;
+              let newMax = e.max !== undefined && e.max !== null ? e.max : this.dataMax;
+              
+              if (newMin !== undefined && newMax !== undefined) {
+                // On recalcule instantanément les 400 points du modèle sur cette nouvelle fenêtre
+                // AVANT que Highcharts ne commence à dessiner la courbe !
+                grapher.redrawAllModels(newMin, newMax);
+              }
+            }
+          },
           afterSetExtremes: function () {
             const grapher = this.chart.options.customGrapherInstance;
             const oldExponent = this.exponent;
@@ -476,21 +491,6 @@ export default class Grapher {
         minPadding: 0,
         maxPadding: 0,
         events: {
-          setExtremes: function (e) {
-            const grapher = this.chart.options.customGrapherInstance;
-            if (grapher && !grapher.suppressModelAutoUpdate) {
-              // On récupère les nouvelles bornes du zoom (e.min et e.max)
-              // Si c'est un "Reset Zoom" (null), on prend les bornes globales des données
-              let newMin = e.min !== undefined && e.min !== null ? e.min : this.dataMin;
-              let newMax = e.max !== undefined && e.max !== null ? e.max : this.dataMax;
-              
-              if (newMin !== undefined && newMax !== undefined) {
-                // On recalcule instantanément les 400 points du modèle sur cette nouvelle fenêtre
-                // AVANT que Highcharts ne commence à dessiner la courbe !
-                grapher.redrawAllModels(newMin, newMax);
-              }
-            }
-          },
           afterSetExtremes: function () {
             const grapher = this.chart.options.customGrapherInstance; // Récupère l'instance
             const oldExponent = this.exponent;
